78b348a541dab29f38fce808f4b28e9a594f9d4d
yahoogm <yaho.garfield@gmail.com>
2022-10-06 21:08:50 +0700

    change UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    fontSize: {
-      sx: " .80rem",
-      date: ".60rem",
-    },
-
-    width: {
-      85: "30rem",
-    },
     extend: {},
   },
   plugins: [],
